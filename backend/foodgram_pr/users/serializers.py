d3b811cdc4dcf43e05df14dcda7450e1bbbdefe3
Timur-x <niktim73431@yandex.ru>
2023-06-21 21:36:13 +0300

    serializer:v3

--- backend/foodgram_pr/users/serializers.py
+++ backend/foodgram_pr/users/serializers.py
@@ -70,4 +70,5 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
     class Meta:
         model = User
-        fields = ('email', 'username', 'first_name', 'last_name', 'password')
+        fields = ('email', 'id', 'username', 'first_name',
+                  'last_name', 'password')
