050feeee3a68ec567bf3f620d3b183cde7a0bb58
Timur-x <niktim73431@yandex.ru>
2023-06-21 21:57:18 +0300

    usermodel

--- backend/foodgram_pr/users/models.py
+++ backend/foodgram_pr/users/models.py
@@ -1,27 +1,25 @@
-from django.contrib.auth.models import (AbstractBaseUser, BaseUserManager,
-                                        PermissionsMixin)
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 
+# class UserManager(BaseUserManager):
+#     def create_user(self, email, username, password, **extra_fields):
+#         if not username:
+#             raise ValueError('Юзернейм не может быть пустым!')
+#         if not email:
+#             raise ValueError('Почта не может быть пустой!')
+#         email = self.normalize_email(email)
+#         user = self.model(email=email, username=username, **extra_fields)
+#         user.set_password(password)
+#         user.save()
+#         return user
 
-class UserManager(BaseUserManager):
-    def create_user(self, email, username, password, **extra_fields):
-        if not username:
-            raise ValueError('Юзернейм не может быть пустым!')
-        if not email:
-            raise ValueError('Почта не может быть пустой!')
-        email = self.normalize_email(email)
-        user = self.model(email=email, username=username, **extra_fields)
-        user.set_password(password)
-        user.save()
-        return user
-
-    def create_superuser(self, email, username, password, **extra_fields):
-        extra_fields.setdefault('is_superuser', True)
-        if not extra_fields.get('is_superuser'):
-            raise ValueError(
-                'Суперпользователь должен иметь is_superuser=True!'
-            )
-        return self.create_user(email, username, password, **extra_fields)
+#     def create_superuser(self, email, username, password, **extra_fields):
+#         extra_fields.setdefault('is_superuser', True)
+#         if not extra_fields.get('is_superuser'):
+#             raise ValueError(
+#                 'Суперпользователь должен иметь is_superuser=True!'
+#             )
+#         return self.create_user(email, username, password, **extra_fields)
 
 
 class User(AbstractBaseUser, PermissionsMixin):
@@ -39,7 +37,7 @@ class User(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['username']
-    objects = UserManager()
+    # objects = UserManager()
 
     class Meta:
         ordering = ('-pk',)
