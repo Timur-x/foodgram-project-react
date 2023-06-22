48a66846461e4e76acc1c502e6570221b4e3915e
Timur-x <niktim73431@yandex.ru>
2023-06-23 00:36:30 +0300

    usersviews:v17

--- backend/foodgram_pr/users/serializers.py
+++ backend/foodgram_pr/users/serializers.py
@@ -26,7 +26,7 @@ class CustomUserSerializer(UserSerializer):
 
     def is_subscribed_user(self, obj):
         user = self.context['request'].user
-        return bool(obj.subscribers.filter(user=user))
+        return bool(obj.subscribes.filter(user=user))
 
     def create(self, validated_data):
         validated_data['password'] = (
