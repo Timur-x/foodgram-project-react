47abf264ca0e5c6678f1e98cd6b2e99793d74ea5
Timur-x <niktim73431@yandex.ru>
2023-06-25 13:14:15 +0300

    cd:v5

--- backend/foodgram_pr/users/views.py
+++ backend/foodgram_pr/users/views.py
@@ -43,7 +43,7 @@ class UserSubscribeViewSet(UserViewSet):
         user_subscriptions = user.subscribers.all()
         authors = [item.author.id for item in user_subscriptions]
         queryset = User.objects.filter(pk__in=authors)
-        paginated_queryset = self.paginate_queryset.all(queryset)
+        paginated_queryset = self.paginate_queryset(queryset)
         serializer = self.get_serializer(paginated_queryset, many=True)
         return self.get_paginated_response(serializer.data)
 
