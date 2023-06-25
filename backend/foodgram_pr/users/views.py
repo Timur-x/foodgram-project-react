6c0fefbaf8c0253948cba6ee54b2f641a18f762a
Timur-x <niktim73431@yandex.ru>
2023-06-25 14:12:26 +0300

    cd:v9

--- backend/foodgram_pr/users/views.py
+++ backend/foodgram_pr/users/views.py
@@ -39,15 +39,16 @@ class UserSubscribeViewSet(UserViewSet):
         permission_classes=(IsAuthenticated, )
     )
     def subscriptions(self, request):
-        user = self.request.user
-        user_subscriptions = user.subscribers.all()
-        authors = [item.author.id for item in user_subscriptions]
-        queryset = User.objects.filter(pk__in=authors)
-        queryset = self.filter_queryset(queryset)
-        paginated_queryset = self.paginate_queryset(queryset).all()
-        serializer = self.get_serializer(paginated_queryset, many=True)
+        # user = self.request.user
+        # user_subscriptions = user.subscribers.all()
+        # authors = [item.author.id for item in user_subscriptions]
+        # queryset = User.objects.filter(pk__in=authors)
+        # queryset = self.filter_queryset(queryset)
+        # paginated_queryset = self.paginate_queryset(queryset).all()
+        # serializer = self.get_serializer(paginated_queryset, many=True)
 
-        return self.get_paginated_response(serializer.data)
+        # return self.get_paginated_response(serializer.data)
+        print('Subscriptions')
 
     @action(
         detail=True,
