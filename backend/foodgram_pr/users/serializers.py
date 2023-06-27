3477485c31997042e318245b8b0044919a275eff
Timur-x <niktim73431@yandex.ru>
2023-06-27 23:17:36 +0300

    starobr:v8

--- backend/foodgram_pr/users/serializers.py
+++ backend/foodgram_pr/users/serializers.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.hashers import make_password
 from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
-from recipes.models import Recipe
+# from recipes.models import Recipe
 from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import SerializerMethodField
 
