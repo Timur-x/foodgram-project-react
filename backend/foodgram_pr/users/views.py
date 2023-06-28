1efb99c52243cd7fcee33ed76b32f942e9de5f53
Timur-x <niktim73431@yandex.ru>
2023-06-28 13:45:29 +0300

    starobr:v18

--- backend/foodgram_pr/users/views.py
+++ backend/foodgram_pr/users/views.py
@@ -41,7 +41,9 @@ class UserSubscribeViewSet(UserViewSet):
         user_subscriptions = user.subscribers.all()
         authors = [item.author.id for item in user_subscriptions]
         queryset = User.objects.filter(pk__in=authors)
-        queryset = self.filter_queryset(queryset).all()
+        if not queryset.exists():  # проверяем, пустой ли queryset
+            return Response({"detail": "No subscriptions found."}, status=200)
+        # queryset = self.filter_queryset(queryset).all()
         paginator = CustomPageNumberPagination
         paginated_queryset = paginator.paginate_queryset(queryset, request)
         serializer = self.get_serializer(paginated_queryset, many=True)
