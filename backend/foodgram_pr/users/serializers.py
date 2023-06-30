942ae08b8165863fb571f6181c59d2604fcf775f
Timur-x <niktim73431@yandex.ru>
2023-06-30 20:53:33 +0300

    userserial

--- backend/foodgram_pr/users/serializers.py
+++ backend/foodgram_pr/users/serializers.py
@@ -1,8 +1,8 @@
-from django.contrib.auth.hashers import make_password
-from django.shortcuts import get_object_or_404
+# from django.contrib.auth.hashers import make_password
+# from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from recipes.models import Recipe
-from rest_framework.exceptions import ValidationError
+# from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import SerializerMethodField
 
 # from rest_framework.validators import UniqueValidator
@@ -17,32 +17,15 @@ class CustomUserSerializer(UserSerializer):
 
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
-        author_id = self.context['request'].data.get('id')
-        author = get_object_or_404(User, pk=author_id)
-        if user == author:
-            raise ValidationError(
-                    'Подписка на самого себя запрещена.'
-                )
-        if Subscription.objects.filter(
-                user=user,
-                author=author
-                 ).exists():
-            raise ValidationError('Подписка уже оформлена.')
 
         if user.is_anonymous:
             return False
 
         return Subscription.objects.filter(user=user, author=obj).exists()
 
-    def create(self, validated_data):
-        validated_data['password'] = (
-            make_password(validated_data.pop('password'))
-        )
-        return super().create(validated_data)
-
     class Meta:
         model = User
-        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+        fields = ('id', 'username', 'first_name', 'last_name', 'email',
                   'is_subscribed')
 
 
