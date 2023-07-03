c6ef15d62860da1d16bf0bce51126fa152b11e03
Timur-x <niktim73431@yandex.ru>
2023-07-03 14:34:18 +0300

    zerview:v16

--- backend/foodgram_pr/users/serializers.py
+++ backend/foodgram_pr/users/serializers.py
@@ -1,41 +1,59 @@
 from django.contrib.auth.hashers import make_password
-# from django.shortcuts import get_object_or_404
+from django.shortcuts import get_object_or_404
 from djoser.serializers import UserCreateSerializer, UserSerializer
 from recipes.models import Recipe
-# from rest_framework.exceptions import ValidationError
+from rest_framework.exceptions import ValidationError
 from rest_framework.serializers import SerializerMethodField
 
 # from rest_framework.validators import UniqueValidator
-from .models import User
+from .models import Subscription, User
 
 
 class CustomUserSerializer(UserSerializer):
-    '''Сериализация User.'''
+    '''Сериализация подписки.'''
     is_subscribed = SerializerMethodField(
         method_name='get_is_subscribed'
     )
 
-    class Meta:
-        model = User
-        fields = (
-            'id', 'email', 'username', 'first_name', 'last_name', 'password',
-            'is_subscribed',
-        )
-        extra_kwargs = {
-            'password': {'write_only': True, 'required': True},
-        }
-
     def get_is_subscribed(self, obj):
         user = self.context['request'].user
-        return (
-            user.is_authenticated and bool(obj.subscribers.filter(user=user))
-         )
+        # if self.context['request'].user.is_authenticated
+        # else:
+        #     None
+        if user.is_anonymous:
+            return False
+        if not user.is_authenticated:
+            return False
+        if not hasattr(user, 'email'):
+            return False
+
+        author_id = self.context['request'].data.get('id')
+        author = get_object_or_404(User, pk=author_id)
+        if user == author:
+            raise ValidationError(
+                    'Подписка на самого себя запрещена.'
+                )
+        if Subscription.objects.filter(
+                user=user,
+                author=author
+                 ).exists():
+            raise ValidationError('Подписка уже оформлена.')
+
+        # if user is None:
+        #     return False
+
+        return Subscription.objects.filter(user=user, author=obj).exists()
 
     def create(self, validated_data):
         validated_data['password'] = (
             make_password(validated_data.pop('password'))
         )
         return super().create(validated_data)
+
+    class Meta:
+        model = User
+        fields = ('email', 'id', 'username', 'first_name', 'last_name',
+                  'is_subscribed')
 
 
 class CustomUserCreateSerializer(UserCreateSerializer):
@@ -47,7 +65,7 @@ class CustomUserCreateSerializer(UserCreateSerializer):
 
 
 class SubscriptionSerializer(CustomUserSerializer):
-    '''Cериализатор для подписок.'''
+    '''Cериализатор для модели User.'''
     recipes = SerializerMethodField(method_name='get_recipes')
     recipes_count = SerializerMethodField(
         method_name='get_recipes_count'
