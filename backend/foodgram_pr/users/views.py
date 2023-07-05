52baf999d359cc98f96791b709566176d3f9fd78
Timur-x <niktim73431@yandex.ru>
2023-07-05 19:59:50 +0300

    userserializert:v14

--- backend/foodgram_pr/users/views.py
+++ backend/foodgram_pr/users/views.py
@@ -1,19 +1,16 @@
 # from django.db.models.signals import post_save
-from django.db import IntegrityError
-from django.http import Http404
 # from django.dispatch import receiver
 from django.shortcuts import get_object_or_404
 from djoser.views import UserViewSet
 # from recipes.models import ShoppingCart
-# from rest_framework import exceptions
+from rest_framework import exceptions
 # from rest_framework.authtoken.models import Token
-# from rest_framework import serializers
 from rest_framework.decorators import action
 from rest_framework.permissions import (IsAuthenticated,
                                         IsAuthenticatedOrReadOnly)
 from rest_framework.response import Response
 from rest_framework.status import (HTTP_201_CREATED, HTTP_204_NO_CONTENT,
-                                   HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND)
+                                   HTTP_405_METHOD_NOT_ALLOWED)
 
 from .models import Subscription, User
 from .pagination import CustomPageNumberPagination
@@ -54,48 +51,43 @@ class UserSubscribeViewSet(UserViewSet):
         return self.get_paginated_response(serializer.data)
 
     @action(
-        methods=('get', 'delete',),
         detail=True,
-        permission_classes=(IsAuthenticated,))
-    def subscribe(self, request, user_id=None):
-        try:
-            author = get_object_or_404(User, pk=user_id)
-        except Http404:
-            return Response(
-                {'detail': 'USER_NOT_FOUND'},
-                status=HTTP_404_NOT_FOUND,
-            )
-        if request.method == 'GET':
-            return self.create_subscribe(request, author)
-        return self.delete_subscribe(request, author)
+        methods=('post', 'delete'),
+        serializer_class=SubscriptionSerializer
+    )
+    def subscribe(self, request, id=None):
+        user = self.request.user
+        author = get_object_or_404(User, pk=id)
 
-    def create_subscribe(self, request, author):
-        if request.user == author:
-            return Response(
-                {'ERRORS_KEY': 'SUBSCRIBE_CANNOT_CREATE_TO_YOURSELF'},
-                status=HTTP_400_BAD_REQUEST,
-            )
-        try:
-            subscribe = Subscription.objects.create(
-                user=request.user,
-                author=author,
-            )
-        except IntegrityError:
-            return Response(
-                {'ERRORS_KEY': 'SUBSCRIBE_CANNOT_CREATE_TWICE'},
-                status=HTTP_400_BAD_REQUEST,
-            )
-        serializer = self.get_subscribtion_serializer(subscribe.author)
-        return Response(serializer.data, status=HTTP_201_CREATED)
+        if self.request.method == 'POST':
+            if user == author:
+                raise exceptions.ValidationError(
+                    'Подписатся на самого себя нельзя.'
+                )
+            if Subscription.objects.filter(
+                user=user,
+                author=author
+            ).exists():
+                raise exceptions.ValidationError('Подписка уже оформлена.')
 
-    def delete_subscribe(self, request, author):
-        try:
-            Subscription.objects.get(user=request.user, author=author).delete()
-        except Subscription.DoesNotExist:
-            return Response(
-                {'ERRORS_KEY': 'SUBSCRIBE_CANNOT_DELETE'},
-                status=HTTP_400_BAD_REQUEST,
+            Subscription.objects.create(user=user, author=author)
+            serializer = self.get_serializer(author)
+
+            return Response(serializer.data, status=HTTP_201_CREATED)
+
+        if self.request.method == 'DELETE':
+            if not user.subscribers.filter(author=author).exists():
+                raise exceptions.ValidationError(
+                    'Подписка уже удалена.'
+                )
+
+            subscription = get_object_or_404(
+                Subscription,
+                user=user,
+                author=author
             )
-        return Response(
-            status=HTTP_204_NO_CONTENT
-        )
+            subscription.delete()
+
+            return Response(status=HTTP_204_NO_CONTENT)
+
+        return Response(status=HTTP_405_METHOD_NOT_ALLOWED)
