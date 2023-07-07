4e760bd432fdc7d0d788d4465f30d02526d2ce96
Timur-x <niktim73431@yandex.ru>
2023-07-07 07:38:23 +0300

    addtag:v3

--- backend/foodgram_pr/recipes/serializers/recipes.py
+++ backend/foodgram_pr/recipes/serializers/recipes.py
@@ -178,11 +178,9 @@ class RecipeCreateUpdateSerializer(ModelSerializer):
 
     def add_tags(self, recipe, tags):
         tag_objs = []
-        for data in tags:
-            tag_data = data.copy()
-            tag = Tag.objects.create(**tag_data)
+        for tag_data in tags:
+            tag = Tag.objects.create(name=tag_data)
             tag_objs.append(tag)
-
         recipe_tags = [RecipeTags(recipe=recipe, tag=tag) for tag in tag_objs]
         RecipeTags.objects.bulk_create(recipe_tags)
 
